9b18cd21687caea825ea5e9566a52f77e01d2dd7
kttito <by.kttito@gmail.com>
2016-01-08 13:51:22 -0500

    perfil

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+	var flag = false;
+	var scroll;
+
+	$(window).scroll(function(){
+		scroll = $(window).scrollTop();
+
+		if(scroll > 100){
+			if(!flag){
+				$("#logo").css({"margin-top": "-5px", "width": "140px","height":"50px"});
+				$("#logo").attr("src","img/logo/icono3.png")
+				$("header").css({"background-color": "rgba(249, 90, 97, 0.7"});
+				flag = true;
+			}
+		}else{
+			if(flag){
+				$("#logo").css({"margin-top": "150px", "width": "630px","height":"157px"});
+				$("#logo").attr("src","img/logo.png")
+				$("header").css({"background-color": "transparent"});
+				flag = false;
+			}
+		}
+	});
+	$( "#logo" ).hover(
+	  function() {
+	  	if($(this).attr('src')=='img/logo.png'){
+	  		$( this ).attr( "src","img/logo2.png");
+	  	}
+	  }, function() {
+	  	if($(this).attr('src')=='img/logo2.png'){
+	  		$( this ).attr( "src","img/logo.png");
+	  	}
+	  }
+	);
+	/*var heighVideo = $("video").height();
+	$("#banner").css({'height':heighVideo+'px'});*/
+	var offsettop = $('.content-about').offset().top-50;
+	$('.btn-pink').click(function(){
+		$('html,body').animate({
+			scrollTop:offsettop
+		},1000);
+	});
+	$( "#profile" ).hover(//borrar desde aqui
+	  function() {
+	  	if($(this).attr('src')=='img/profile.jpg'){
+	  		$( this ).attr( "src","img/profile2.png");
+	  	}
+	  }, function() {
+	  	if($(this).attr('src')=='img/profile2.png'){
+	  		$( this ).attr( "src","img/profile.jpg");
+	  	}
+	  }
+	);//borrarrrrr
+});
